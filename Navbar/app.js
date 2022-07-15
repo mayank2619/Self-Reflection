53225a979a73d0306a10bd55bafcb04a93239248
mayank2619 <mayanksharma2619@gmail.com>
2022-07-15 14:27:02 +0530

    Add files via upload

---
+++ Navbar/app.js
@@ -0,0 +1,21 @@
+// const clicked = document.querySelector(".navbar-toggler");
+// const links= document.querySelector(".links");
+//
+// clicked.addEventListener("click", function(){
+//
+// links.classList.toggle("show-links");
+//
+// })
+
+
+
+document.getElementById('buttonID').onclick= function(){
+
+myFunction();
+
+
+}
+function myFunction(){
+
+  document.getElementById("content").classList.toggle("show");
+}
